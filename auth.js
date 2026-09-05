313e2d88eb360a6ee8e7b9ee13a16fdfe62bbe19
faiz2025-ui <faizhamad35@gmail.com>
2026-09-05 19:47:03 +0100

    Add files via upload

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@
 
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db');
+const db = require('./db');
 
 // POST /api/auth/login
 router.post('/login', async (req, res) => {
